37ee654f69e687b41499204a4516d53f297ea525
cyjin463 <cyjin463@hanmail.net>
2022-02-28 16:35:27 +0900

    회원가입 기능 구현

---
+++ src/redux/Signup.js
@@ -0,0 +1,107 @@
+import React from "react";
+import styled from "styled-components";
+import { useDispatch } from "react-redux";
+import { actionsCreators as userActions} from "../redux/modules/user"
+
+function Signup () {
+    const dispatch = useDispatch();
+
+    const [username, setUsername] = React.useState("");
+    const [nickname, setNickname] = React.useState("");
+    const [password, setPassword] = React.useState("");
+    const [checkPassword, setCheckPassword] = React.useState("");
+    const [isCheckUsername, setIsCheckUsername] = React.useState(false);
+    const [isCheckNickname, setIsCheckNickname] = React.useState(false);
+
+    const CheckUsername = () => {
+        if(username === "") {
+            window.alert("아이디를 입력해주세요.")
+            return;
+        }
+        console.log(username, "중복요청");
+        dispatch(userActions.isCheckUsernameDB(username, false));
+        setIsCheckUsername(true);
+    }
+
+    const checkNickname = () => {
+        if (nickname === "") {
+            window.alert("닉네임이 공란입니다.")
+            return;
+        }
+
+        console.log(nickname, "의 중복확인 요청을 dispatch 했습니다.");
+        dispatch(userActions.checkNicknameDB(nickname, false));
+        setIsCheckNickname(true)
+    }
+
+    const signup = (e) => {
+        e.preventDefault()
+
+        setIsCheckNickname(true)
+        setIsCheckUsername(true)
+        console.log(isCheckUsername, isCheckNickname)
+        if (username === "" || nickname === "" || password === "") {
+            window.alert("빈칸을 채워주세요")
+            return;
+        } else if (password !== checkPassword) {
+            window.alert("패스워드가 다릅니다.")
+            return;
+        } else if (isCheckUsername === false || isCheckNickname === false) {
+            window.alert("중복확인을 해주세요.")
+            return;
+        }
+        dispatch(userActions.signupDB(username, nickname, password));
+        setIsCheckUsername(false);
+        setIsCheckNickname(false);
+    }
+
+    return (
+        <>
+            <div>
+                <div>
+                    <span>아이디</span>
+                    <input
+                    type="text"
+                    placeholder="ID"
+                    onChange={(e) => {
+                        setUsername(e.target.value);
+                    }}
+                    />
+                    <button onClick={CheckUsername} >중복체크</button>
+                </div>
+                <div>
+                    <span>닉네임</span>
+                    <input
+                    type="text"
+                    placeholder="Nickname"
+                    onChange={(e) => {
+                        setNickname(e.target.value);
+                    }}
+                    />
+                    <button onClick={checkNickname} >중복체크</button>
+                </div>
+                <div>
+                    <span>비밀번호</span>
+                    <input
+                    type="password"
+                    placeholder="password"
+                    onChange={(e) => {
+                        setPassword(e.target.value);
+                    }}
+                    />
+                </div>
+                <div>
+                    <span>아이디</span>
+                    <input
+                    type="password"
+                    placeholder="passwordCheck"
+                    onChange={(e) => {
+                        setCheckPassword(e.target.value);
+                    }}
+                    />
+                </div>
+            </div>
+        
+        </>
+    )
+}
---
+++ src/redux/user.js
@@ -0,0 +1,86 @@
+import { createAction, handleActions } from "redux-actions";
+import { produce } from "immer";
+import history from "../configureStore"
+import axios from "axios";
+
+//initialState
+const initialState = {
+    user: null,
+    is_login: false,
+    isCheckUsername: false,
+    isCheckNickname: false,
+};
+
+//action
+const CHECK_USERNAME = "CHECK_USERNAME";
+const CHECK_NICKNAME = "CHECK_NICKNAME";
+
+//action creators
+const setCheckUsername = createAction(CHECK_USERNAME, (isCheckUsername) => ({ isCheckUsername }))
+const setCheckNickname = createAction(CHECK_NICKNAME, (isCheckNickname) => ({ isCheckNickname }))
+
+//middleware actions
+const checkUsernameDB = (username, isCheckUsername) => {
+    return async function (dispatch, getState, {history}) {
+        console.log(username, isCheckUsername);
+        await axios.post("/user/signup/username", { "username" : username }
+        ).then((res) => {
+            if (res.data === false) {
+                window.alert("이미 존재하는 ID입니다.");
+                return;
+            }
+            dispatch(setCheckUsername(!isCheckUsername));
+        })
+    }
+}
+
+const checkNicknameDB = (userNickname, isCheckNickname) => {
+    return async function (dispatch, getState, { history }) {
+        await axios.post("/user/signup/nickname", { "userNickname" : userNickname }
+        ).then((res) => {
+            if (res.data === false) {
+                window.alert("이미 존재하는 닉네임 입니다.");
+                return;
+            }
+            dispatch(setCheckNickname(!isCheckNickname));
+        })
+    }
+}
+
+const signupDB = (username, userNickname, userPwd) => {
+    return function (dispatch, getState, {history}) {
+        axios
+        .post('/user/signup', {
+            "username" : username,
+            "userNickname" : userNickname,
+            "userPwd" : userPwd,
+        })
+        .then((res) => {
+            window.alert("회원가입을 축하드립니다.");
+            history.push("/");
+        });
+    };
+};
+
+export default handleActions (
+    {
+        [CHECK_USERNAME] : (state, action) => produce(state, (draft) => {
+            console.log("CHECK_USERNAME 리듀서 적용", state, action.payload);
+            draft.isCheckUsername = action.payload.isCheckUsername;
+            window.alert("사용 가능한 ID입니다.");
+        }),
+        [CHECK_NICKNAME] : (state, action) => produce(state, (draft) => {
+            console.log("CHECK_NICKNAME 리듀서 적용", state, action.payload);
+            draft.isCheckNickname = action.payload.isCheckNickname;
+            window.alert("사용 가능한 닉네임입니다.");
+        })
+    }
+);
+
+const actionsCreators = {
+    signupDB,
+    checkUsernameDB,
+    checkNicknameDB,
+};
+
+export { actionsCreators };
