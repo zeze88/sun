e8aaac4046b61821e4ff9d75e3fb1cc64e3378ca
재연 <charmant1402@gmail.com>
2022-03-10 10:23:23 +0900

    게시글 디테일 페이지 마크업 수정

--- src/page/Detail.js
+++ src/page/Detail.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import styled from 'styled-components';
-import Comment from '../components/Comment';
-import { useParams } from 'react-router';
-import { useSelector, useDispatch } from 'react-redux';
-import { actionCreators as postActions } from '../redux/modules/post';
-import { history } from '../redux/configureStore';
-import Answer from '../components/Answer';
-import AnswerList from '../components/AnswerList';
+import React from "react";
+import styled from "styled-components";
+import Comment from "../components/Comment";
+import { useParams } from "react-router";
+import { useSelector, useDispatch } from "react-redux";
+import post, { actionCreators as postActions } from "../redux/modules/post";
+import { history } from "../redux/configureStore";
+import Answer from "../components/Answer";
+import AnswerList from "../components/AnswerList";
 const Detail = () => {
+  const dispatch = useDispatch();
   const pid = useParams().pid;
   const post_one = useSelector((state) => state.post.list);
-  const dispatch = useDispatch();
-  const user_info = sessionStorage.getItem('uid');
+  const user_info = sessionStorage.getItem("uid");
+
   const delPost = () => {
+    alert();
     dispatch(postActions.delPostDB(pid));
   };
 
@@ -24,55 +26,79 @@ const Detail = () => {
     dispatch(postActions.getOnePostDB(pid));
   }, []);
 
+  console.log(post_one);
   return (
     <Container>
-      <button onClick={likebtn}>관심</button>
-      {user_info === post_one.uid && ''}
-      <button styled={{ fontSize: '40px' }} onClick={delPost}>
-        삭제
-      </button>
-      <button
-        styled={{ fontSize: '40px' }}
-        onClick={() => {
-          history.push(`/edit/${pid}`);
-        }}>
-        수정
-      </button>
       <Question>
         <Top>
           <div
             style={{
-              display: 'flex',
-              justifyContent: 'space-between',
-              alignItems: 'center',
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
             }}>
-            <div style={{ fontSize: '2rem', fontWeight: '700' }}>
+            <div style={{ fontSize: "2rem", fontWeight: "700" }}>
               {post_one.postTitle}
             </div>
-            <div style={{ textAlign: 'right', marginRight: '2rem' }}>
+            <div style={{ textAlign: "right", marginRight: "2rem" }}>
               <div>💻 Img</div>
               화면공유
             </div>
           </div>
           <div
             style={{
-              display: 'flex',
-              justifyContent: 'space-between',
-              margin: '1rem 2rem 0px 0px',
+              display: "flex",
+              justifyContent: "space-between",
+              margin: "1rem 2rem 0px 0px",
             }}>
-            <span>조회수 작성일</span>
-            {post_one.tag?.map((v, idx) => (
-              <div key={idx} style={{ minWidth: '2rem', border: '1px solid ' }}>
+            <dl>
+              <dt>
+                <img src={post_one.userImage} alt='프로필 이미지' />
+              </dt>
+              <dd>{post_one.nickname}</dd>
+            </dl>
+            <em>작성시간이 없어요</em>
+            {/* {post_one.tag?.map((v, idx) => (
+              <div key={idx} style={{ minWidth: "2rem", border: "1px solid " }}>
                 {v}
               </div>
-            ))}
+            ))} */}
           </div>
         </Top>
-        <Line />
-        <img src={post_one.postImg} />
-        <ContentBox>
-          <Content> {post_one.postComment}</Content>
-        </ContentBox>
+        <ContextWrap>
+          <p>{post_one.postComment}</p>
+          <div>
+            <img src={post_one.postImg} />
+          </div>
+
+          <div>
+            <button onClick={likebtn}>관심</button>
+            <button styled={{ fontSize: "40px" }} onClick={delPost}>
+              삭제
+            </button>
+            <button
+              styled={{ fontSize: "40px" }}
+              onClick={() => {
+                history.push(`/edit/${pid}`);
+              }}>
+              수정
+            </button>
+            {user_info === post_one.uid && (
+              <>
+                <button styled={{ fontSize: "40px" }} onClick={delPost}>
+                  삭제
+                </button>
+                <button
+                  styled={{ fontSize: "40px" }}
+                  onClick={() => {
+                    history.push(`/edit/${pid}`);
+                  }}>
+                  수정
+                </button>
+              </>
+            )}
+          </div>
+        </ContextWrap>
       </Question>
       <Answer />
       <AnswerList />
@@ -104,6 +130,8 @@ const Top = styled.div`
   height: 16%;
   padding: 1rem;
 `;
+
+const ContextWrap = styled.div``;
 
 const Line = styled.hr`
   color: #d3d3d3;
