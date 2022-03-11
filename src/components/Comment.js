7cc1c8c6a4fdfb191a05a2bd83667bc45fb8045f
cyjin463 <cyjin463@hanmail.net>
2022-03-11 21:23:35 +0900

    브랜치 업데이트

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,92 +1,86 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
+import React, { useState } from "react";
+import styled from "styled-components";
 import { useSelector, useDispatch } from "react-redux";
 import { actionCreators as commentActions } from "../redux/modules/comment";
-import answer from '../redux/modules/answer';
-import { useParams } from 'react-router-dom';
+import answer from "../redux/modules/answer";
+import { useParams } from "react-router-dom";
 
 const Comment = () => {
+  const dispatch = useDispatch();
+  const pid = useParams().pid;
+  const uid = sessionStorage.getItem("uid");
+  const state = useSelector((state) => state);
+  const answerId = "1";
+  const [comment, setComment] = React.useState("");
+  console.log(uid);
+  console.log(state.answer);
+  console.log(pid);
 
-    const dispatch = useDispatch();
-    const pid = useParams().pid;
-    const uid = sessionStorage.getItem("uid");
-    const state = useSelector((state) => state)
-    const answerId = "1"
-    const [comment, setComment] = React.useState("");
-    console.log(uid)
-    console.log(state.answer)
-    console.log(pid)
+  // React.useEffect(() => {
+  //     dispatch()
+  // })
 
-    // React.useEffect(() => {
-    //     dispatch()
-    // })
+  const addComment = () => {
+    dispatch(commentActions.addCommentDB(uid, pid, answerId, comment));
+  };
 
-    const addComment = () => {
-        dispatch(commentActions.addCommentDB(uid, pid, answerId, comment))
-    }
+  const onChange = (e) => {
+    setComment(e.target.value);
+    console.log(comment);
+  };
 
-    const onChange = (e) => {
-        setComment(e.target.value);
-        console.log(comment)
-    }
-
-    return (
-        <Container >
-            <CommentCount>
-            댓글 0 개
-            </CommentCount>
-            <CommentInput>
-                <CommentWrite onChange={onChange} />
-            </CommentInput>
-            <Buttom onClick={addComment}>
-                입력하기
-            </Buttom>
-        </Container>
-    );
+  return (
+    <Container>
+      <CommentCount>댓글 0 개</CommentCount>
+      <CommentInput>
+        <CommentWrite onChange={onChange} />
+      </CommentInput>
+      <Buttom onClick={addComment}>입력하기</Buttom>
+    </Container>
+  );
 };
 
 const Container = styled.div`
-width : 70%;
-height : 8rem;
-background-color : #D3D3D3;
-border-radius : 1rem;
-display : flex;
-margin-top : 2rem;
+  width: 70%;
+  height: 8rem;
+  background-color: #d3d3d3;
+  border-radius: 1rem;
+  display: flex;
+  margin-top: 2rem;
 `;
 
 const CommentCount = styled.div`
-width : 4rem;
-padding : 1rem 0px 0px 2rem;
+  width: 4rem;
+  padding: 1rem 0px 0px 2rem;
 `;
 
 const CommentInput = styled.div`
-width : 100%;
-height : 100%;
-display : flex;
-align-items : center;
-margin-left : 2rem;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  margin-left: 2rem;
 `;
 
 const CommentWrite = styled.input`
-width : 95%;
-height : 50%;
-border : 0px solid black; 
-border-radius : 1rem;
+  width: 95%;
+  height: 50%;
+  border: 0px solid black;
+  border-radius: 1rem;
 `;
 
 const Buttom = styled.button`
-width : 5rem;
-height : 1.5rem;
-position : absolute;
-font-size : 1rem;
-color : black; 
-text-align : center;
-padding : 0px;
-border-radius : 1rem;
-margin : 4% 0px 0px 62%;
-background-color : #676767;
-cursor: pointer;
+  width: 5rem;
+  height: 1.5rem;
+  position: absolute;
+  font-size: 1rem;
+  color: black;
+  text-align: center;
+  padding: 0px;
+  border-radius: 1rem;
+  margin: 4% 0px 0px 62%;
+  background-color: #676767;
+  cursor: pointer;
 `;
-
 
 export default Comment;
---
+++ src/page/Mypage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Edituser from "../components/Edituser";
+
+const Mypage = () => {
+  return (
+    <div>
+      <Edituser />
+    </div>
+  );
+};
+
+export default Mypage;
