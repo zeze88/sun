6e9bbadbf4f3fc425f764600c4981e97b9c8df12
재연 <charmant1402@gmail.com>
2022-03-17 18:18:17 +0900

    버튼 라인 버전 추가

--- src/elements/RoundBtn.js
+++ src/elements/RoundBtn.js
@@ -1,18 +1,34 @@
 import React from "react";
 import styled from "styled-components";
 
-const RoundBtn = ({ onClick, title }) => {
-  return <Button onClick={onClick}>{title}</Button>;
+const RoundBtn = ({ onClick, title, isLine = false }) => {
+  return (
+    <Button onClick={onClick} isLine={isLine}>
+      {title}
+    </Button>
+  );
 };
 
 const Button = styled.button`
-  line-height: 30px;
+  --main-color: #7966ff;
+  height: 39px;
   flex: none;
   padding: 0 32px;
   color: #fff;
   font-size: 16px;
   border-radius: 32px;
-  background-color: #7966ff;
+  box-size: border-box;
+  ${(props) =>
+    props.isLine
+      ? `
+    color:var(--main-color);
+    border:solid 3px var(--main-color);
+    background-color:#fff;
+    
+    `
+      : `
+    background-color: var(--main-color);
+  `};
 `;
 
 export default RoundBtn;
