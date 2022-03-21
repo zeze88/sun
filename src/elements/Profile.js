ca2ab0ac2ea5ab40aba5ce842c89873b0ffc42f3
재연 <charmant1402@gmail.com>
2022-03-22 01:07:02 +0900

    프로필 디폴트 이미지 추가

--- src/elements/Profile.js
+++ src/elements/Profile.js
@@ -1,12 +1,21 @@
 import React from "react";
 import styled from "styled-components";
+import { ReactComponent as UserDefault } from "../svg/logo-1.svg";
 
 const Profile = ({ imgUrl, size = "30" }) => {
-  return <ProfileImg imgUrl={imgUrl} size={size}></ProfileImg>;
+  return (
+    <ProfileImg imgUrl={imgUrl} size={size}>
+      {!imgUrl && <UserDefault />}
+    </ProfileImg>
+  );
 };
 
 const ProfileImg = styled.span`
   display: inline-block;
+  svg {
+    width: 100%;
+    height: 100%;
+  }
   ${(props) =>
     props.size &&
     `
@@ -14,10 +23,7 @@ const ProfileImg = styled.span`
   height: ${props.size}px;
   border-radius:${props.size}px;
   `}
-  background: url(${(props) =>
-    props.imgUrl
-      ? props.imgUrl
-      : "https://t1.daumcdn.net/cfile/tistory/21340A3650ED95850C"})
+  background: url(${(props) => props.imgUrl && props.imgUrl})
     no-repeat center / cover;
 `;
 
