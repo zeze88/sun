9ee67aa6f44c918914f0de0a2acf3b61dc2e0e75
재연 <charmant1402@gmail.com>
2022-03-23 00:46:13 +0900

    del: 필요 없는 코드 파일 삭제

--- src/redux/modules/arams.js
+++
@@ -1,8 +0,0 @@
-import { createAction, handleActions } from "redux-actions";
-import produce from "immer";
-
-const ARAM = "ARAM";
-const aram = createAction(ARAM, (list) => ({ list }));
-const initialState = {
-  status: "",
-};
--- src/redux/modules/comment.js
+++ src/redux/modules/comment.js
@@ -1,4 +1,3 @@
-import React from "react";
 import axios from "axios";
 import { createAction, handleActions } from "redux-actions";
 import produce from "immer";
