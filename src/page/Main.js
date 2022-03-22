e93c8f4d0000b7ac479cc231a3aa1c2831a6d90b
재연 <charmant1402@gmail.com>
2022-03-22 11:47:49 +0900

    베너 추가

--- src/page/Main.js
+++ src/page/Main.js
@@ -2,12 +2,16 @@ import React from "react";
 import styled from "styled-components";
 import Chat from "../components/Chat";
 import NoticeBoard from "../components/NoticeBoard";
+import Banner from "../elements/Banner";
 
 const Main = () => {
   return (
     <Container>
       <NoticeBoard />
-      <Chat />
+      <div className='right'>
+        <Chat />
+        <Banner />
+      </div>
     </Container>
   );
 };
@@ -17,6 +21,11 @@ const Container = styled.div`
   max-width: 1440px;
   margin: 0 auto;
   gap: 24px;
+
+  .right {
+    flex: none;
+    width: 342px;
+  }
 `;
 
 export default Main;
