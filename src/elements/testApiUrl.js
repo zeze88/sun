3f7bd7ccf3621732c5102014e06a60157cc45bed
cyjin463 <cyjin463@hanmail.net>
2022-03-24 02:58:20 +0900

    fix : AnswerList comment 수정

--- src/elements/testApiUrl.js
+++ src/elements/testApiUrl.js
@@ -1,6 +1,8 @@
 // export const apiUrl = "http://13.125.211.125";
 // export const apiUrl = "http://15.164.231.31";
-export const apiUrl = "http://175.117.251.202:7050";
+// export const apiUrl = "http://175.117.251.202:7050";
+export const apiUrl = "http://15.165.74.177";
+
 // export const apiUrl = "http://175.118.48.164:7050";
 // export const apiUrl = "http://52.78.190.242";
 // export const apiUrl = "http://221.150.182.158:7050";
