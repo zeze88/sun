03eaf91cb2e9697197615fc7be6aa412cbe51735
cyjin463 <cyjin463@hanmail.net>
2022-03-28 10:21:37 +0900

    fix :  로그인 화면 min 추가

--- src/page/Login.js
+++ src/page/Login.js
@@ -105,8 +105,10 @@ const Login = () => {
 };
 
 const Container = styled.div`
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  min-width: 1440px;
+  height: 100%;
+  min-height: 1080px;
   display: flex;
   justify-content: center;
 `;
