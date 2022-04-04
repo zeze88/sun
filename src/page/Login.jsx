fb0c5af9200f415340d5a3ea3fbbee98046bf4f0
재연 <charmant1402@gmail.com>
2022-04-04 17:32:55 +0900

    test 성공

--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -51,7 +51,7 @@ const Login = () => {
                   name='select'
                   value='login'
                 />
-                로그인 성공~!
+                로그인
               </label>
               <label className={check === "signup" ? "signup" : ""}>
                 <input
